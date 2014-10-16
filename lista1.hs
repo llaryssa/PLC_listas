81a5be84b4a4aa18123ddffd1e9010f86c4424f2
Laryssa Seabra <laryssa.seabra@gprt.ufpe.br>
2014-10-16 11:15:28 -0300

    mais uma questao

--- lista1.hs
+++ lista1.hs
@@ -31,3 +31,19 @@ gerarCombinacoes n (x:xs) t = map (x:) (gerarCombinacoes (n-1) xs t)
 gerarChaveDiffieHellman :: Integer -> Integer -> Integer -> Integer -> Integer
 gerarChaveDiffieHellman p g a b = mod (aa ^ b) p where aa = mod (g ^ a) p 
 -- ex: gerarChaveDiffieHellman 97 5 36 58
+
+
+-- questão 6
+
+filtroGerarPares :: [(String, Char, Bool)] -> Char -> [(String)]
+filtroGerarPares [] pref = []
+filtroGerarPares ((nome, sexo, quer):xs) pref 
+	| quer == False   = filtroGerarPares xs pref
+	| sexo == pref    = (nome) : filtroGerarPares xs pref
+	| pref == 'x'     = (nome) : filtroGerarPares xs pref
+	| pref == 'v'     = filtroGerarPares ((nome, sexo, quer):xs) 'h'
+	| otherwise       = filtroGerarPares xs pref
+
+list = [("Glaucio", 'h', True), ("Zeno", 'h', False), ("Josiela", 'm', True), ("Juliana", 'm', False), ("Priscillo", 'm', True), ("Robson", 'm', False)]
+pref = 'm'
+result = filtroGerarPares list pref

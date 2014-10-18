5455fc5fbc64444a80214a14f57e6ec626a2e649
Geeosp <geeo.sp@gmail.com>
2014-10-18 14:14:26 -0300

    Do jeito que a gente deixou

--- L1Q1.hs
+++ L1Q1.hs
@@ -21,18 +21,51 @@ dc = dias até a colheita
 
 
 -}
+-- fi insetos -> inteiro
+folhasdepoisdeInsetos::Int->[(Int,(Int,Int))]->Int
 folhasdepoisdeInsetos fi [] = fi
 folhasdepoisdeInsetos fi (((fd,(fmin,fmax)):xs))
 											|(fmin<=fi&&fmax>=fi)  = folhasdepoisdeInsetos (fi - fd) xs
 											|otherwise = folhasdepoisdeInsetos fi xs
+
+folhasInsetos:: Int->(Int,Int)->[(Int,(Int,Int))]->Int
 folhasInsetos dias  (fi, fd) [] = fi + dias*fd
 folhasInsetos dias  (fi, fd) insetos 
 						| (dias == 0) = fi
-						| otherwise = folhasInsetos (dias-1) (folhasdepoisdeInsetos ((fi + fd) insetos ),fd) insetos
+						| otherwise = folhasInsetos (dias-1) ((folhasdepoisdeInsetos (fi + fd) insetos ),fd) insetos
 
+
+folhaspulverizadas:: (Int, Int) ->Int-> Int
 folhaspulverizadas (fi, fd) dias = fi + fd*dias
 
+
+q1:: Int->Int->[(Int,Int)]->[(Int,(Int,Int))]->Int->[(Int,Int)]
 q1 vv vp [] li dc = []
 q1 vv vp (p:ps) li dc 
-		|((folhaspulverizadas p dc)*vv -vp)< vv*(folhasInsetos dc p li) = p:(q1 vv vp ps li dc)		
-		|otherwise = (q1 vv vp ps li dc)		
+		|((folhaspulverizadas p dc)*vv -vp) >= vv*(folhasInsetos dc p li) = p:(q1 vv vp ps li dc)		
+		|otherwise = (q1 vv vp ps li dc)
+
+
+
+valorvenda = 2
+valorpulv = 12
+plantas = [(5, 3), (15, 0), (10, 2), (5, 5), (0, 7)]
+insetos = [(2, (2, 5)), (1, (12, 20)), (2, (5, 10)), (2, (3, 10)), (2, (10,20))]
+dias = 2
+
+resultado = q1 valorvenda valorpulv plantas insetos dias
+
+{--
+1. 8, 8, 8, 6, 4, 4 | 7, 7, 7, 5, 3, 3              6
+2. 15, 15, 14, 14, 14, 12 | 12, 12, 11, 11, 11, 9   18
+3. 12, 12, 11, 11, 11, 9 | 11, 11, 11, 11, 11, 9    18
+4. 10, 10, 9, 7, 5, 5 | 10, 10, 10, 8, 6, 6         12          
+5. 7, 7, 7, 5, 3, 3 | 10, 10, 10, 8, 6, 6           12
+
+1. 8, 11     22 - 12 = 10
+2. 15, 15    30 - 12 = 18
+3. 12, 14    28 - 12 = 16
+4. 10, 15    30 - 12 = 18
+5. 7 14      28 - 12 = 16
+
+--}
